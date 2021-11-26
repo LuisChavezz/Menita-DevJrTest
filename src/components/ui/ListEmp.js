9b007336731361659b3f204fd63088797bb2f323
LuisChavezz <ing.luischavezz@gmail.com>
2021-11-26 00:04:03 -0600

    diseño de los botones de la tabla

--- src/components/ui/ListEmp.js
+++ src/components/ui/ListEmp.js
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import { FaUserEdit } from 'react-icons/fa';
+import { BsFillTrashFill } from 'react-icons/bs';
 
 export const ListEmp = ({ nombre, departamento, status } ) => {
     
@@ -10,8 +11,20 @@ export const ListEmp = ({ nombre, departamento, status } ) => {
             <td>{ nombre }</td>
             <td>{ departamento }</td>
             <td>{ status }</td>
-            <td>--editar--</td>
-            <td>--eliminar--</td>
+            <td>
+                <button
+                    className="ListEmp__main__table__edit"
+                >
+                    <FaUserEdit clashName="ListEmp__main__table__edit__icon"/>
+                </button>
+            </td>
+            <td>
+                <button
+                    className="ListEmp__main__table__delete"
+                >
+                    <BsFillTrashFill clashName="ListEmp__main__table__edit__icon" />
+                </button>
+            </td>
         </tr>
     )
 }
