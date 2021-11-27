7a52efe4eff7e892da73beaa79e6fc226d3e3d37
LuisChavezz <ing.luischavezz@gmail.com>
2021-11-26 21:32:18 -0600

    Filtrado de la lista de empleados

--- src/components/screens/ListEmpScreen.js
+++ src/components/screens/ListEmpScreen.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { FaFilter } from 'react-icons/fa';
 
 import { ListEmp } from '../ui/ListEmp';
 
@@ -7,10 +8,64 @@ import { ListEmp } from '../ui/ListEmp';
 export const ListEmpScreen = () => {
     
     const {empleados} = useSelector(state => state.workers);
-    // console.log(empleados);
+
+    const [empleadosTabla, setEmpleados] = useState(empleados);
+    const [filtros, setFiltros] = useState({
+        dep:  '',
+        stat: '',
+    });
+
+    useEffect(() => {
+        console.log(filtros)
+        if ( filtros.dep === '' && filtros.stat === '' ) {
+            setEmpleados(empleados);
+        
+        } else if ( filtros.dep !== '' && filtros.stat === '' ) {
+            setEmpleados( empleados.filter( emp => (emp.departamento === filtros.dep)) )
+
+        } else if ( filtros.dep === '' && filtros.stat !== '' ) {
+            setEmpleados( empleados.filter( emp => (emp.status === filtros.stat)) )
+            
+        } else if ( filtros.dep !== '' && filtros.stat !== '' ) {
+            setEmpleados( empleados.filter( emp => (emp.departamento === filtros.dep) && (emp.status === filtros.stat)) )
+            
+        }
+
+    }, [filtros])
+
     
     return (
         <div className="ListEmp__main">
+            <div className="ListEmp__filters">
+                <div>
+                    <p><FaFilter />Departamento</p>
+                    <select 
+                        name="departamento"
+                        onChange={ (e) => setFiltros({...filtros, dep: e.target.value}) }
+                    >
+                        <option value="">*</option>
+                        <option value="Recursos Humanos">Recursos Humanos</option>
+                        <option value="Sistemas">Sistemas</option>
+                        <option value="Contabilidad">Contabilidad</option>
+                        <option value="Credito y Cobranza">Credito y Cobranza</option>
+                        <option value="Nómina">Nómina</option>
+                        <option value="Legal">Legal</option>
+                    </select>
+                </div>
+
+                <div>
+                    <p><FaFilter />Status</p>
+                    <select 
+                        name="status"
+                        onChange={ (e) => setFiltros({...filtros, stat: e.target.value}) }
+                    >
+                        <option value="">*</option>
+                        <option value="Activo">Activo</option>
+                        <option value="Inactivo">Inactivo</option>
+                    </select>
+                </div>
+            </div>
+
             <table className="ListEmp__main__table">
                 <thead>
                     <tr>
@@ -23,7 +78,7 @@ export const ListEmpScreen = () => {
                 </thead>
                 <tbody>
                     {
-                         empleados.map( emp => (
+                         empleadosTabla.map( emp => (
                             <ListEmp 
                                 key={ emp.rfc } 
                                 { ...emp } // PROP de cada propiedad que contenga cada 'Note'
